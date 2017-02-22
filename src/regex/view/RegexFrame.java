280148060638b677617f091b2a508f3b4ae1df34
MarkGrayson <Mpal7487@Gmail.com>
2017-02-22 13:24:13 -0700

    Set up frame

--- src/regex/view/RegexFrame.java
+++ src/regex/view/RegexFrame.java
@@ -1,7 +1,28 @@
 package regex.view;
 
 import regex.controller.*;
-public class RegexFrame 
-{
+import javax.swing.JFrame;
+import java.awt.Dimension;
 
+public class RegexFrame extends JFrame
+{
+	private RegexController baseController;
+	private RegexPanel appPanel;
+	
+	public RegexFrame(RegexController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		this.appPanel = new RegexPanel(baseController);
+		
+		this.setupRegexFrame();
+	}
+	
+	public void setupRegexFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle(" ");
+		this.setSize(new Dimension(700, 700));
+		this.setVisible(true);
+	}
 }
